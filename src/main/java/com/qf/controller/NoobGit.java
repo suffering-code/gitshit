6da619a655ef908e38c54c96b373332d6c92b442
fyl <478868087@qq.com>
2019-09-26 20:44:15 +0800

    noobgit

---
+++ src/main/java/com/qf/controller/NoobGit.java
@@ -0,0 +1,11 @@
+package com.qf.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class NoobGit {
+
+    public static void main(String[] args) {
+        System.out.println("'noobGitt");
+    }
+}
